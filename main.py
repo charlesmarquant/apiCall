02e17c225e33c75e890826cc199fc40404fda433
charlesmarquant <marquantcharles@gmail.com>
2024-03-14 13:15:09 -0400

    add comment

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 import pandas as pd
 
+## Call API
 response = requests.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
 
+## Put json data into a table
 json_data = response.json()
 table_data = pd.json_normalize(json_data['results'])
 
+## Print table
 print(table_data)
